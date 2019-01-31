fadb430b67fc87744b35bc5b3f9a53d21b20b04d
chandanamg <saichandanachowdary@gmail.com>
2019-01-31 16:58:29 +0530

    added datacontroller

--- src/main/java/com/ibm/newsletter/controller/DataController.java
+++ src/main/java/com/ibm/newsletter/controller/DataController.java
@@ -3,6 +3,12 @@ package com.ibm.newsletter.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class DataController {
+	
+	public String getData() {
+		return null;
+		
+	}
 
 }
